394e12e9139abf52062de56e88e5cb7954871d98
ysnr8811 <86781060+ysnr8811@users.noreply.github.com>
2025-04-15 23:17:44 +0900

    サイドドロワーの編集、共通化
    サイドドロワーコンポーネントのディレクトリを移動
    
    layoutに記述を移動して共通化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import MaterialDrawer from "./components/drawer"; // クライアントコンポーネント
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* サイドドロワーを共通表示 */}
+        <MaterialDrawer />
+
+        {/* ページごとの内容 */}
+        <main>{children}</main>
       </body>
     </html>
   );
